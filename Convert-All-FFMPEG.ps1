aca9e07450560b64515863d2a13708838c50f413
Jason Struckhoff <jstruckhoff@users.noreply.github.com>
2024-01-29 18:06:06 +0700

    Fixed bug when processing filenames with spaces

--- Convert-All-FFMPEG.ps1
+++ Convert-All-FFMPEG.ps1
@@ -1,45 +1,47 @@
 function Convert-All-FFMPEG {
-	param (
-    [Parameter(Mandatory=$true, Position=0)]
+  param (
+    [Parameter(Mandatory = $true, Position = 0)]
     [ValidateNotNullOrEmpty()]
     [string]$InputExt,
 
-    [Parameter(Mandatory=$true, Position=1)]
+    [Parameter(Mandatory = $true, Position = 1)]
     [ValidateNotNullOrEmpty()]
     [string]$OutputExt,
 
-    [Parameter(Mandatory=$false, Position=2)]
+    [Parameter(Mandatory = $false, Position = 2)]
     [string]$OptionalSettings,
 
-    [Parameter(Mandatory=$false, Position=3)]
+    [Parameter(Mandatory = $false, Position = 3)]
     [bool]$DeleteOriginalFile
   )
 
-	$Files = Get-ChildItem -Filter ("*." + $InputExt) -Recurse
+  $Files = Get-ChildItem -Filter ("*." + $InputExt) -Recurse
   if ($Files.Length -eq 0) {
     Write-Host "No files found matching extension" $InputExt -ForegroundColor Yellow
     Return
   }
   $ConvertDirectory = New-Item -Name "Converted" -ItemType "directory" -Force
-	foreach ($File in $Files) {
-    		$NewFile = [io.path]::ChangeExtension($ConvertDirectory.FullName + '\' + $File.Name, ("." + $OutputExt))
-        if ($OptionalSettings) {
-          $ArgumentList = "-i {0} {1} {2}" -f $File.FullName, $OptionalSettings, $NewFile
-        }
-        else {
-          $ArgumentList = "-i {0} {1}" -f $File.FullName, $NewFile
-        }
-        $Split = $ArgumentList.Split(' ')
-        & "ffmpeg.exe" $Split    		
-        if ($LASTEXITCODE -eq 0) {
-          Write-Host "Conversion Successful" -ForegroundColor Green
-          if ($DeleteOriginalFile) {
-            Remove-Item -Path $File
-          }
-        }
-        else {
-          Write-Host "Conversion Failed" -ForegroundColor Red
-          Return $LASTEXITCODE
-        }
-	}
+  foreach ($File in $Files) {
+    $NewFile = [io.path]::ChangeExtension($ConvertDirectory.FullName + '\' + $File.Name, ("." + $OutputExt))
+    $ArgumentList = [System.Collections.ArrayList]@()
+    $ArgumentList.Add("-i")
+    $ArgumentList.Add("{0}" -f $File.FullName)
+    if ($OptionalSettings -ne "") {
+      foreach ($OptionalSetting in $OptionalSettings.Split(" ")) {
+        $ArgumentList.Add($OptionalSetting)
+      }
+    }
+    $ArgumentList.Add("{0}" -f $NewFile)
+    & "ffmpeg.exe" $ArgumentList    		
+    if ($LASTEXITCODE -eq 0) {
+      Write-Host "Conversion Successful" -ForegroundColor Green
+      if ($DeleteOriginalFile) {
+        Remove-Item -Path $File
+      }
+    }
+    else {
+      Write-Host "Conversion Failed" -ForegroundColor Red
+      Return $LASTEXITCODE
+    }
+  }
 }
